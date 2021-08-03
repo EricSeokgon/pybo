6b6e2f0befeb24d6ac4d9a61cdf5d4d23adabf7b
hadesyi <leesk55@gmail.com>
2021-08-04 05:25:15 +0900

    파이보 프로젝트 최초 커밋

---
+++ config/settings/prod.py
@@ -0,0 +1,17 @@
+from .base import *
+
+ALLOWED_HOSTS = ['52.78.8.100', 'pybo.kr']
+STATIC_ROOT = BASE_DIR / 'static/'
+STATICFILES_DIRS = []
+DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pybo',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '=eKmx$xxymnxxwNCxxx$SX55*RdjKK1G&',
+        'HOST': 'ls-be78fd2cxxxxx614420dxxxxx6b156e2c9.cqlcyugj7ibs.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
